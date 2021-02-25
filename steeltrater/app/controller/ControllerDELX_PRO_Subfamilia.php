9ff9cfacc9fb02764bb53fdd55a84d3a76979417
diferete <33422296+diferete@users.noreply.github.com>
2021-02-25 08:46:02 -0300

    Update ControllerDELX_PRO_Subfamilia.php
    Adicionados filtros para persistência conforme atualização de filtro dinâmico

--- steeltrater/app/controller/ControllerDELX_PRO_Subfamilia.php
+++ steeltrater/app/controller/ControllerDELX_PRO_Subfamilia.php
@@ -18,16 +18,31 @@ class ControllerDELX_PRO_Subfamilia extends Controller {
 
         $aCampos = array();
         parse_str($_REQUEST['campos'], $aCampos);
-        if (count($aCampos) > 0) {
-            if(isset($aCampos['pro_grupocodigofin'])){
-                $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo'],0, Persistencia::ENTRE,$aCampos['pro_grupocodigofin']); 
-                $this->Persistencia->adicionaFiltro('pro_subgrupocodigo', $aCampos['pro_subgrupocodigo'],0, Persistencia::ENTRE,$aCampos['pro_subgrupocodigofin']);
-                $this->Persistencia->adicionaFiltro('pro_familiacodigo', $aCampos['pro_familiacodigo'],0, Persistencia::ENTRE,$aCampos['pro_familiacodigofin']);
-           }else{
-            $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo']);
-            $this->Persistencia->adicionaFiltro('pro_subgrupocodigo', $aCampos['pro_subgrupocodigo']);
-            $this->Persistencia->adicionaFiltro('pro_familiacodigo', $aCampos['pro_familiacodigo']);
-           }
+        $aFiltro = $_REQUEST['parametrosCampos'];
+        sort($aFiltro);
+        if (count($aCampos) > 0 || count($aFiltro) > 0) {
+            if (isset($aCampos['pro_grupocodigofin'])) {
+                $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo'], 0, Persistencia::ENTRE, $aCampos['pro_grupocodigofin']);
+                $this->Persistencia->adicionaFiltro('pro_subgrupocodigo', $aCampos['pro_subgrupocodigo'], 0, Persistencia::ENTRE, $aCampos['pro_subgrupocodigofin']);
+                $this->Persistencia->adicionaFiltro('pro_familiacodigo', $aCampos['pro_familiacodigo'], 0, Persistencia::ENTRE, $aCampos['pro_familiacodigofin']);
+            } else {
+                if (count($aCampos) > 0) {
+                    $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo']);
+                    $this->Persistencia->adicionaFiltro('pro_subgrupocodigo', $aCampos['pro_subgrupocodigo']);
+                    $this->Persistencia->adicionaFiltro('pro_familiacodigo', $aCampos['pro_familiacodigo']);
+                } else {
+                    $aFiltro = explode(',', $aFiltro[0]);
+                    if ($aFiltro[0] != '') {
+                        $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aFiltro[0]);
+                    }
+                    if ($aFiltro[1] != '') {
+                        $this->Persistencia->adicionaFiltro('pro_subgrupocodigo', $aFiltro[1]);
+                    }
+                    if ($aFiltro[2] != '') {
+                        $this->Persistencia->adicionaFiltro('pro_familiacodigo', $aFiltro[2]);
+                    }
+                }
+            }
         }
     }
 
