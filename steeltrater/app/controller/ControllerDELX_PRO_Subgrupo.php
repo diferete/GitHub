bd6f6335889b4ef604e47733917c4e779e761954
diferete <33422296+diferete@users.noreply.github.com>
2021-02-25 08:46:11 -0300

    Update ControllerDELX_PRO_Subgrupo.php
    Adicionados filtros para persistência conforme atualização de filtro dinâmico

--- steeltrater/app/controller/ControllerDELX_PRO_Subgrupo.php
+++ steeltrater/app/controller/ControllerDELX_PRO_Subgrupo.php
@@ -18,25 +18,31 @@ class ControllerDELX_PRO_Subgrupo extends Controller {
 
         $aCampos = array();
         parse_str($_REQUEST['campos'], $aCampos);
-        if (count($aCampos) > 0) {
-           if(isset($aCampos['pro_grupocodigofin'])){
-             $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo'],0, Persistencia::ENTRE,$aCampos['pro_grupocodigofin']); 
-          }else{
-             $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo']);   
-          }
+        $aFiltro = $_REQUEST['parametrosCampos'];
+        sort($aFiltro);
+        if (count($aCampos) > 0 || count($aFiltro) > 0) {
+            if (isset($aCampos['pro_grupocodigofin'])) {
+                $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo'], 0, Persistencia::ENTRE, $aCampos['pro_grupocodigofin']);
+            } else {
+                if (count($aCampos) > 0) {
+                    $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo']);
+                } else {
+                    if ($aFiltro[0] != '') {
+                        $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aFiltro[0]);
+                    }
+                }
+            }
         }
     }
-    
+
     public function antesValorBuscaPk() {
         parent::antesValorBuscaPk();
-        
+
         $aCampos = array();
         parse_str($_REQUEST['campos'], $aCampos);
         if (count($aCampos) > 0) {
             $this->Persistencia->adicionaFiltro('pro_grupocodigo', $aCampos['pro_grupocodigo']);
         }
-        
     }
-    
-    
+
 }
