bd361abbc4736af529bb256903c01656b32bb56b
diferete <33422296+diferete@users.noreply.github.com>
2021-03-11 14:14:06 -0300

    Update ViewSTEEL_PCP_ReceitasItens.php
    Adicionados campos CamadaEspessura; TempoZinc; PesoDoCesto. Algumas endentações foram feitas assim como foram incluidos nos campos os parâmetros de tamanhos para telas menores

--- steeltrater/app/view/ViewSTEEL_PCP_ReceitasItens.php
+++ steeltrater/app/view/ViewSTEEL_PCP_ReceitasItens.php
@@ -15,20 +15,26 @@ class ViewSTEEL_PCP_ReceitasItens extends View {
         $oSeq = new CampoConsulta('Seq.', 'seq');
 
         $oTrat = new CampoConsulta('Tratamento', 'STEEL_PCP_Tratamentos.tratcod');
+        $oTrat->setILargura(10);
 
         $oTratDes = new CampoConsulta('Desc', 'STEEL_PCP_Tratamentos.tratdes');
-
-        $oCamadaMin = new CampoConsulta('CamadaMín', 'camada_min', CampoConsulta::TIPO_DECIMAL);
-
-        $oCamadaMax = new CampoConsulta('CamadaMáx', 'camada_max', CampoConsulta::TIPO_DECIMAL);
+        $oTratDes->setILargura(300);
 
         $oTemperatura = new CampoConsulta('Temperatura', 'temperatura', CampoConsulta::TIPO_DECIMAL);
         $oTemperatura->addComparacao('', CampoConsulta::COMPARACAO_DIFERENTE, CampoConsulta::COL_VERDE, CampoConsulta::MODO_COLUNA, false, '');
         $oTemperatura->setBComparacaoColuna(true);
-        
+
         $oRecApont = new CampoConsulta('Apontamento Produção', 'recApont');
 
-        $this->addCampos($oCod, $oSeq, $oTrat, $oTratDes, $oTemperatura, $oRecApont);
+        $oCamadaEspessura = new CampoConsulta('Camada Espessura', 'CamadaEspessura', CampoConsulta::TIPO_DECIMAL);
+
+        $oTempoZinc = new CampoConsulta('Tempo Zincagem', 'TempoZinc');
+        $oTempoZinc->setILargura(10);
+
+        $oPesoDoCesto = new CampoConsulta('Peso do Cesto', 'PesoDoCesto', CampoConsulta::TIPO_DECIMAL);
+        $oPesoDoCesto->setILargura(10);
+
+        $this->addCampos($oCod, $oSeq, $oTrat, $oTratDes, $oTemperatura, $oRecApont, $oCamadaEspessura, $oTempoZinc, $oPesoDoCesto);
     }
 
     public function criaGridDetalhe() {
@@ -44,19 +50,26 @@ class ViewSTEEL_PCP_ReceitasItens extends View {
         $oSeq = new CampoConsulta('Seq.', 'seq');
 
         $oTrat = new CampoConsulta('Tratamento', 'STEEL_PCP_Tratamentos.tratcod');
+        $oTrat->setILargura(10);
 
         $oTratDes = new CampoConsulta('Desc', 'STEEL_PCP_Tratamentos.tratdes');
-
-        $oCamadaMin = new CampoConsulta('CamadaMín', 'camada_min', CampoConsulta::TIPO_DECIMAL);
-
-        $oCamadaMax = new CampoConsulta('CamadaMáx', 'camada_max', CampoConsulta::TIPO_DECIMAL);
+        $oTratDes->setILargura(300);
 
         $oTemperatura = new CampoConsulta('Temperatura', 'temperatura', CampoConsulta::TIPO_DECIMAL);
         $oTemperatura->addComparacao('', CampoConsulta::COMPARACAO_DIFERENTE, CampoConsulta::COL_VERDE, CampoConsulta::MODO_COLUNA, false, '');
         $oTemperatura->setBComparacaoColuna(true);
+
         $oRecApont = new CampoConsulta('Apontamento Produção', 'recApont');
 
-        $this->addCamposDetalhe($oCod, $oSeq, $oTrat, $oTratDes, $oTemperatura, $oRecApont);
+        $oCamadaEspessura = new CampoConsulta('Camada Espessura', 'CamadaEspessura', CampoConsulta::TIPO_DECIMAL);
+
+        $oTempoZinc = new CampoConsulta('Tempo Zincagem', 'TempoZinc');
+        $oTempoZinc->setILargura(10);
+
+        $oPesoDoCesto = new CampoConsulta('Peso do Cesto', 'PesoDoCesto', CampoConsulta::TIPO_DECIMAL);
+        $oPesoDoCesto->setILargura(10);
+
+        $this->addCamposDetalhe($oCod, $oSeq, $oTrat, $oTratDes, $oTemperatura, $oRecApont, $oCamadaEspessura, $oTempoZinc, $oPesoDoCesto);
         $this->addGriTela($this->getOGridDetalhe());
     }
 
@@ -66,18 +79,18 @@ class ViewSTEEL_PCP_ReceitasItens extends View {
         $this->criaGridDetalhe();
         $aValor = $this->getAParametrosExtras();
 
-        $oCod = new Campo('Código', 'cod', Campo::TIPO_TEXTO, 1);
+        $oCod = new Campo('Código', 'cod', Campo::TIPO_TEXTO, 1, 1, 12, 12);
         $oCod->setSValor($aValor[0]);
         $oCod->setBCampoBloqueado(true);
-        $oSeq = new Campo('Seq.', 'seq', Campo::TIPO_TEXTO, 1);
+        $oSeq = new Campo('Seq.', 'seq', Campo::TIPO_TEXTO, 1, 1, 12, 12);
         $oSeq->setBCampoBloqueado(true);
 
-        $oTrat = new Campo('Tratamento', 'STEEL_PCP_Tratamentos.tratcod', Campo::TIPO_BUSCADOBANCOPK, 2);
+        $oTrat = new Campo('Tratamento', 'STEEL_PCP_Tratamentos.tratcod', Campo::TIPO_BUSCADOBANCOPK, 2, 2, 12, 12);
         $oTrat->setSIdHideEtapa($this->getSIdHideEtapa());
         $oTrat->setBFocus(true);
         $oTrat->addValidacao(false, Validacao::TIPO_STRING, '', '1', '100');
 
-        $oTratDes = new Campo('Desc', 'STEEL_PCP_Tratamentos.tratdes', Campo::TIPO_BUSCADOBANCO, 4);
+        $oTratDes = new Campo('Desc', 'STEEL_PCP_Tratamentos.tratdes', Campo::TIPO_BUSCADOBANCO, 4, 4, 12, 12);
         $oTratDes->setSIdPk($oTrat->getId());
         $oTratDes->setClasseBusca('STEEL_PCP_Tratamentos');
         $oTratDes->addCampoBusca('tratcod', '', '');
@@ -85,23 +98,34 @@ class ViewSTEEL_PCP_ReceitasItens extends View {
         $oTratDes->setSIdTela($this->getTela()->getid());
         $oTratDes->addValidacao(false, Validacao::TIPO_STRING, '', '2', '100');
 
-
         $oTrat->setClasseBusca('STEEL_PCP_Tratamentos');
         $oTrat->setSCampoRetorno('tratcod', $this->getTela()->getId());
         $oTrat->addCampoBusca('tratdes', $oTratDes->getId(), $this->getTela()->getId());
 
-        $oCamadaMin = new Campo('CamadaMín', 'camada_min', Campo::TIPO_TEXTO, 1);
-        $oCamadaMax = new Campo('CamadaMáx', 'camada_max', Campo::TIPO_TEXTO, 1);
-        $oTemperatura = new Campo('Temperatura', 'temperatura', Campo::TIPO_TEXTO, 1);
-        $oTempo = new Campo('Tempo', 'tempo', Campo::TIPO_TEXTO, 1);
-        $oResf = new Campo('Resfriamento', 'resfriamento', Campo::TIPO_TEXTO, 4);
+        $oCamadaMin = new Campo('CamadaMín', 'camada_min', Campo::TIPO_TEXTO, 1, 1, 12, 12);
 
-        $oRecApont = new Campo('Recebe Apontamento de Produção', 'recApont', Campo::CAMPO_SELECTSIMPLE, 3);
+        $oCamadaMax = new Campo('CamadaMáx', 'camada_max', Campo::TIPO_TEXTO, 1, 1, 12, 12);
+
+        $oTemperatura = new Campo('Temperatura', 'temperatura', Campo::TIPO_TEXTO, 1, 1, 12, 12);
+
+        $oTempo = new Campo('Tempo', 'tempo', Campo::TIPO_TEXTO, 1, 1, 12, 12);
+
+        $oResf = new Campo('Resfriamento', 'resfriamento', Campo::TIPO_TEXTO, 4, 4, 12, 12);
+
+        $oRecApont = new Campo('Recebe Apontamento de Produção', 'recApont', Campo::CAMPO_SELECTSIMPLE, 3, 3, 12, 12);
         $oRecApont->addItemSelect('SIM', 'SIM');
         $oRecApont->addItemSelect('NÃO', 'NÃO');
 
+        $oCamadaEspessura = new Campo('Camada Espessura', 'CamadaEspessura', Campo::TIPO_DECIMAL, 2, 2, 12, 12);
+
+        $oTempoZinc = new Campo('Tempo Zincagem', 'TempoZinc', Campo::TIPO_TEXTO, 1, 1, 12, 12);
+        $oTempoZinc->setBTime(true);
+
+        $oPesoDoCesto = new Campo('Peso do Cesto', 'PesoDoCesto', Campo::TIPO_DECIMAL_COMPOSTO, 1, 1, 12, 12);
+        $oPesoDoCesto->setICasaDecimal(3);
+
         /* Botão para inserir no banco de dados */
-        $oBotConf = new Campo('Inserir', '', Campo::TIPO_BOTAOSMALL_SUB, 1);
+        $oBotConf = new Campo('Inserir', '', Campo::TIPO_BOTAOSMALL_SUB, 1, 1, 12, 12);
         $sGrid = $this->getOGridDetalhe()->getSId();
         //id form,id incremento,id do grid, id focus,    
         $sAcao = 'requestAjax("' . $this->getTela()->getId() . '-form","' . $this->getController() . '","acaoDetalheIten",'
@@ -110,7 +134,7 @@ class ViewSTEEL_PCP_ReceitasItens extends View {
         $this->getTela()->setIdBtnConfirmar($oBotConf->getId());
         $this->getTela()->setAcaoConfirmar($sAcao);
 
-        $this->addCampos(array($oCod, $oSeq), array($oTrat, $oTratDes), array($oTempo, $oResf, $oTemperatura, $oRecApont, $oBotConf));
+        $this->addCampos(array($oCod, $oSeq), array($oTrat, $oTratDes, $oCamadaEspessura, $oTempoZinc, $oPesoDoCesto), array($oTempo, $oResf, $oTemperatura, $oRecApont, $oBotConf));
 
         //adiciona objetos campos para servirem como filtros iniciais do grid
         $this->addCamposFiltroIni($oCod);
