91d578f8b9def68649eb0926ffaaeab0f43b3c85
diferete <33422296+diferete@users.noreply.github.com>
2021-03-11 14:02:35 -0300

    Update ControllerSTEEL_PCP_OrdensFabItens.php
    Alterado o método para inserir os itens da ordem de produção para verificar se op igual somente zincagem e não carrega nas etapas e criado método itensOrdemZincagem para inserir os itens da OP

--- steeltrater/app/controller/ControllerSTEEL_PCP_OrdensFabItens.php
+++ steeltrater/app/controller/ControllerSTEEL_PCP_OrdensFabItens.php
@@ -16,14 +16,26 @@ class ControllerSTEEL_PCP_OrdensFabItens extends Controller {
 
     //método para inserir os itens da ordem de produção
     public function itensOrdem($oModelFab) {
+        $aReceitaItens = array();
         $sReceita = $oModelFab->getReceita();
+        $sReceitaZinc = $oModelFab->getReceita_zinc();
         $this->Persistencia->adicionaFiltro('op', $oModelFab->getOp());
         $this->Persistencia->excluir();
         $this->Persistencia->limpaFiltro();
 
         //retorna a receita do item
         $oReceita = Fabrica::FabricarController('STEEL_PCP_ReceitasItens');
-        $aReceitaItens = $oReceita->retornaReceita($sReceita);
+        //verifica se op igual somente zincagem não carrega nas etapas 
+        if($oModelFab->getTipoOrdem()!=='Z'){
+            $aReceitaItens = $oReceita->retornaReceita($sReceita);
+        }
+        if($sReceitaZinc!=0){
+            $oReceita1 = Fabrica::FabricarController('STEEL_PCP_ReceitasItens');
+            $aReceitaItensZinc = $oReceita1->retornaReceita($sReceitaZinc);
+            foreach ($aReceitaItensZinc as $key1){
+                array_push($aReceitaItens,$key1);
+            }
+        }
         $key = 1;
         foreach ($aReceitaItens as $key => $oReceitaItens) {
             $key = $key + 1;
@@ -41,6 +53,51 @@ class ControllerSTEEL_PCP_OrdensFabItens extends Controller {
             $this->Persistencia->inserir();
         }
     }
+    
+     //método para inserir os itens da ordem de produção
+    public function itensOrdemZincagem($oModelFab) {
+        $aReceita = array();
+        $this->Persistencia->adicionaFiltro('op', $oModelFab->getOp());
+        $aArray = $this->Persistencia->getArrayModel();
+        $oReceitas = Fabrica::FabricarController('STEEL_PCP_Receitas');
+        foreach ($aArray as $key){
+            
+            $oReceitas->Persistencia->adicionaFiltro('cod', $key->getReceita());
+            $aReceita = $oReceitas->Persistencia->getArrayModel();
+            $oModelReceita = $aReceita[0];
+            if($oModelReceita->getTipoReceita()=="Zincagem"){
+                $this->Persistencia->adicionaFiltro('op', $oModelFab->getOp());
+                $this->Persistencia->adicionaFiltro('receita', $oModelReceita->getCod());
+                $this->Persistencia->excluir();
+                $this->Persistencia->limpaFiltro();
+            }
+            $oReceitas->Persistencia->limpaFiltro();
+        }
+        
+        $aReceitaItensZinc = array();
+        $this->Persistencia->adicionaFiltro('op', $oModelFab->getOp());
+        $aReceita = $this->Persistencia->getArrayModel();
+        $key = count($aReceita);
+        $sReceitaZinc = $oModelFab->getReceita_zinc();
+        $oReceita1 = Fabrica::FabricarController('STEEL_PCP_ReceitasItens');
+        $aReceitaItensZinc = $oReceita1->retornaReceita($sReceitaZinc);
+        foreach ($aReceitaItensZinc as $oReceitaItens) {
+            $key = $key + 1;
+            $this->Model->setOp($oModelFab->getOp());
+            $this->Model->setOpseq($key);
+            $this->Model->setReceita($oReceitaItens->getCod());
+            $this->Model->setReceita_seq($oReceitaItens->getSeq());
+            $this->Model->setTratamento($oReceitaItens->getSTEEL_PCP_Tratamentos()->getTratcod());
+            $this->Model->setCamada_min($oReceitaItens->getCamada_min());
+            $this->Model->setCamada_max($oReceitaItens->getCamada_max());
+            $this->Model->setTemperatura($oReceitaItens->getTemperatura());
+            $this->Model->setTempo($oReceitaItens->getTempo());
+            $this->Model->setResfriamento($oReceitaItens->getResfriamento());
+
+            $this->Persistencia->inserir();
+        }
+    }
+    
 
     public function antesDeCriarConsulta($sParametros = null) {
         parent::antesDeCriarConsulta($sParametros);
