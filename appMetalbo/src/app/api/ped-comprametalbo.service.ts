21cc41c95ba69c22db29324e33795c24892a0681
diferete <33422296+diferete@users.noreply.github.com>
2021-04-12 13:40:15 -0300

    Create ped-comprametalbo.service.ts
    Criada para pegar os dados de cabeçalho e itens da solicitação de compra

---
+++ appMetalbo/src/app/api/ped-comprametalbo.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+import { ConexaoService } from './conexao.service';
+import { LoadingController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedCompraMetalboService {
+
+  loading: any;
+
+  constructor(private http: HttpClient,
+    public conexao: ConexaoService,
+    public loadingController: LoadingController) { }
+
+  //MENSAGEM DE LOADING
+  async presentLoading(message: string) {
+
+    this.loading = await this.loadingController.create({
+      message
+    });
+    return this.loading.present();
+  }
+
+  getPedCompras(usutoken, usucod, mes, cnpj) {
+    this.presentLoading('');
+    let dadosEnv = {
+      classe: "STEEL_SUP_PedidoCompra",
+      metodo: "getDadosPedidoCompras", dados: {
+        "mes": mes,
+        "cnpj": cnpj,
+      },
+      usucodigo: usucod,
+      usutoken: usutoken
+    };
+
+    return new Promise((resolve, reject) => {
+      this.http.post(this.conexao.link, dadosEnv)
+        .subscribe((result: any) => {
+          setTimeout(() => {
+            this.loading.dismiss();
+          });
+          resolve(result);
+
+        },
+          (error) => {
+            setTimeout(() => {
+              this.loading.dismiss();
+            });
+            reject('Sem conexão!');
+          });
+    });
+
+
+  }
+
+}
