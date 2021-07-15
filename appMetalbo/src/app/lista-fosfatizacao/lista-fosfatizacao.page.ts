348766955907479f98416d21b7ceb7d5ddda926a
diferete <33422296+diferete@users.noreply.github.com>
2021-07-15 09:34:34 -0300

    Update lista-fosfatizacao.page.ts
    Identações

--- appMetalbo/src/app/lista-fosfatizacao/lista-fosfatizacao.page.ts
+++ appMetalbo/src/app/lista-fosfatizacao/lista-fosfatizacao.page.ts
@@ -7,98 +7,93 @@ import { AlertController } from '@ionic/angular';
 import { StorageService } from '../api/storage.service';
 import { LoadingController } from '@ionic/angular';
 
-
 @Component({
   selector: 'app-lista-fosfatizacao',
   templateUrl: './lista-fosfatizacao.page.html',
   styleUrls: ['./lista-fosfatizacao.page.scss'],
 })
 export class ListaFosfatizacaoPage implements OnInit {
-    dataMes: string;
-    data: any;
-    total: any;
-    
+  dataMes: string;
+  data: any;
+  total: any;
 
-    listaDiario = [];
+  listaDiario = [];
 
-    constructor(public router: Router,
-        public menu: MenuController,
-        private route: ActivatedRoute,
-        public fatMetalboService: FatMetalboService,
-        public alertController: AlertController,
-        public StorageServ: StorageService,
-        public loadingController: LoadingController) {
-            this.data = new Date();
-            this.dataMes = new Date().toDateString();
-    }
-
-    //mensagem internet
-    async mensagemAlertInternet() {
-        const alert = await this.alertController.create({
-            header: 'Atenção!',
-            subHeader: 'Verifique sua conexão com a internet.',
-            message: 'Dados não ativos.',
-            buttons: ['OK'],
-        });
-
-        await alert.present();
-    }
-
-  ngOnInit() {
+  constructor(
+    public router: Router,
+    public menu: MenuController,
+    private route: ActivatedRoute,
+    public fatMetalboService: FatMetalboService,
+    public alertController: AlertController,
+    public StorageServ: StorageService,
+    public loadingController: LoadingController
+  ) {
+    this.data = new Date();
+    this.dataMes = new Date().toDateString();
   }
 
-    ionViewWillEnter() {
-        this.total = '0';
-        this.getLista();
-    }
+  //mensagem internet
+  async mensagemAlertInternet() {
+    const alert = await this.alertController.create({
+      header: 'Atenção!',
+      subHeader: 'Verifique sua conexão com a internet.',
+      message: 'Dados não ativos.',
+      buttons: ['OK'],
+    });
 
-    //pull do refresh
-    doRefresh(event) {
-        this.getLista();
+    await alert.present();
+  }
 
-        setTimeout(() => {
-            console.log('Async operation has ended');
-            event.target.complete();
-        }, 2000);
-    }
+  ngOnInit() {}
 
-    //chama função para carrega os dados
-    getLista() {
-       
-        //variáveis usuário e token
-        let usutoken;
-        let usucod;
+  ionViewWillEnter() {
+    this.total = '0';
+    this.getLista();
+  }
 
-        this.StorageServ.retornaToken()
-            .then((result: any) => {
-                usutoken = result;
-                return this.StorageServ.retornaUsuCod();
-            }).then((result: any) => {
-                usucod = result;
-               // console.log('data tela ' + this.dataMes);
-                return this.fatMetalboService.getListaEsperaFosfatizacao(usutoken, usucod, this.dataMes);
-            }).then((result: any) => {
-                // this.listaDiario = result.DADOS.total;
-                this.total = result.DADOS.total;
-                this.listaDiario = result.DADOS.lista;
-              
-               // console.log('total php ' + result.DADOS.lista);
-            });
+  //pull do refresh
+  doRefresh(event) {
+    this.getLista();
 
-    }
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 
-    //abre tela detalhe
-    getDadosDetalhe(dataconv) {
-       
-        let navigationExtras: NavigationExtras = {
-            state: {
-                valorParaEnviar: dataconv
-            }
-        };
-        console.log(navigationExtras);
-        this.router.navigate(['lista-fofatizacao-detalhe'], navigationExtras);
-    }
+  //chama função para carrega os dados
+  getLista() {
+    //variáveis usuário e token
+    let usutoken;
+    let usucod;
 
+    this.StorageServ.retornaToken()
+      .then((result: any) => {
+        usutoken = result;
+        return this.StorageServ.retornaUsuCod();
+      })
+      .then((result: any) => {
+        usucod = result;
+        // console.log('data tela ' + this.dataMes);
+        return this.fatMetalboService.getListaEsperaFosfatizacao(usutoken, usucod, this.dataMes);
+      })
+      .then((result: any) => {
+        // this.listaDiario = result.DADOS.total;
+        this.total = result.DADOS.total;
+        this.listaDiario = result.DADOS.lista;
 
+        // console.log('total php ' + result.DADOS.lista);
+      });
+  }
 
+  //abre tela detalhe
+  getDadosDetalhe(dataconv) {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        valorParaEnviar: dataconv,
+      },
+    };
+    console.log(navigationExtras);
+    this.router.navigate(['lista-fofatizacao-detalhe'], navigationExtras);
+  }
 }
