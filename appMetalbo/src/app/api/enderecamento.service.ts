e346b916c976f9420773ff6a067e86efaa2686d5
diferete <33422296+diferete@users.noreply.github.com>
2021-09-21 08:27:51 -0300

    Create enderecamento.service.ts

---
+++ appMetalbo/src/app/api/enderecamento.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
+import { ConexaoService } from './conexao.service';
+import { LoadingController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EnderecamentoService {
+  loading: any;
+
+  constructor(
+    private http: HttpClient,
+    private toastController: ToastController,
+    public conexao: ConexaoService,
+    public loadingController: LoadingController
+  ) {}
+
+  //MENSAGEM DE LOADING
+
+  async presentLoading() {
+    this.loading = await this.toastController.create({
+      message: 'Carregando!',
+      color: 'dark',
+      duration: 9000,
+    });
+    return this.loading.present();
+  }
+  getDadosEnderecamento(usutoken, usucod, estante, coluna, nivel, tipo, codigo) {
+    this.presentLoading();
+    let dadosEnv = {
+      classe: 'MET_EST_Enderecamento',
+      metodo: 'getDadosEnderecamento',
+      dados: {
+        usucodigo: usucod,
+        estante: estante,
+        coluna: coluna,
+        nivel: nivel,
+        tipo: tipo,
+        codigo: codigo,
+      },
+      usucodigo: usucod,
+      usutoken: usutoken,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.http.post(this.conexao.link, dadosEnv).subscribe(
+        (result: any) => {
+          setTimeout(() => {
+            this.loading.dismiss();
+          });
+          resolve(result);
+        },
+        (error) => {
+          setTimeout(() => {
+            this.loading.dismiss();
+          });
+          reject('Sem conexão!');
+        }
+      );
+    });
+  }
+
+  updateEndereco(usutoken, usucod, estante, nivel, coluna, tipo, codigo, armcod) {
+        this.presentLoading();
+    let dadosEnv = {
+      classe: 'MET_EST_Enderecamento',
+      metodo: 'updateEndereco',
+      dados: {
+        usucodigo: usucod,
+        estante: estante,
+        nivel: nivel,
+        coluna: coluna,
+        tipo: tipo,
+        codigo: codigo,
+        armcod: armcod,
+      },
+      usucodigo: usucod,
+      usutoken: usutoken,
+    };
+    return new Promise((resolve, reject) => {
+      this.http.post(this.conexao.link, dadosEnv).subscribe(
+        (result: any) => {
+          setTimeout(() => {
+            this.loading.dismiss();
+          });
+          resolve(result);
+        },
+        (error) => {
+          setTimeout(() => {
+            this.loading.dismiss();
+          });
+          reject('Sem conexão!');
+        }
+      );
+    });
+  }
+}
